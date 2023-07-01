02d90a974bfd39f38666cfef7274b5d1d34376cb
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2023-07-01 21:51:56 +0200

    Deploy

---
+++ dist/assets/ArrivalsView-a6be546c.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c as a}from"./index-075206f2.js";const c={};function s(n,o){return r(),a("div",null,"This is an arrivals page")}const i=e(c,[["render",s]]);export{i as default};
