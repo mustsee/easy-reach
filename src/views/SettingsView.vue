64c9dc68a27223ddd26b60547e478240c3861684
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2023-07-20 20:36:12 +0200

    wip: add custom messages on settings page

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,11 +1,15 @@
 <script setup>
 import SenderName from './../components/settings/SenderName.vue'
+import Messages from './../components/settings/Messages.vue'
 </script>
 
 <template>
   <div class="my-8">
     <Suspense>
-      <SenderName />
+      <div class="grid grid-flow-row gap-6">
+        <SenderName />
+        <Messages />
+      </div>
       <template #fallback>
         <div>loading...</div>
       </template>
