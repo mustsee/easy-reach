aa475e2f726cc8c15f001b889471aa1c1fc4c1ff
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2024-06-09 22:33:11 +0200

    deploy

--- src/components/FooterMenu.vue
+++ src/components/FooterMenu.vue
@@ -1,5 +1,5 @@
 <template>
   <footer class="text-center border-t border-gray-200 bg-gray-900 py-2">
-    <span class="text-white">2023</span>
+    <span class="text-white">2024</span>
   </footer>
 </template>
