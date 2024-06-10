a4942f8dab57157dc588e663ce06b9eebc42acb0
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2024-06-10 18:32:49 +0200

    Change

--- src/stores/clickCounterStore.js
+++ src/stores/clickCounterStore.js
@@ -37,7 +37,7 @@ export const useClickCounterStore = defineStore('click-counter', {
       const date = now.toLocaleDateString().split('/').reverse().join('')
       const time = now.toLocaleTimeString()
       try {
-        await add(`logs/${date}/${this.logNames[name]}`, {user: userStore.userEmail(), time, navigator: navigator.userAgent})
+        await add(`logs/${date}/${this.logNames[name]}`, {user: userStore.userEmail, time, navigator: navigator.userAgent})
         /* toast('Success writing log', {
           position: toast.POSITION.BOTTOM_RIGHT,
           autoClose: true,
